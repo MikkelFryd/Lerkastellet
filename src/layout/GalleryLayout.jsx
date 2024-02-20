6ec9cd37182eda872f8e927eaf65ec7c90913def
mikkelfryd <mikk7021@gmail.com>
2024-02-20 10:33:33 +0100

    Update

--- src/layout/GalleryLayout.jsx
+++ src/layout/GalleryLayout.jsx
@@ -43,6 +43,7 @@ export const GalleryLayout = () => {
         <Button
           fontSize={["sm", "md", "lg", "xl"]}
           w={"100%"}
+          fontStyle={"300"}
           p={"1em"}
           color={"white"}
           colorScheme="gray"
